4dbfdac06dd48db5cc81b254a74504343ead7bf5
Amirali <amiralikazempour14@gmail.com@gmail.com>
2026-06-09 15:39:18 +0330

    Initial commit: setup + intro card

---
+++ intro.py
@@ -0,0 +1,9 @@
+#intro.py -card-e moarefi man
+
+print("==========")
+print("card-e moarefi man") 
+print("==========")
+print("esm:amirali")
+print("shoghl:businessman") 
+print("chera python:cash")
+print("hadaf:sakht application hesabdari")
