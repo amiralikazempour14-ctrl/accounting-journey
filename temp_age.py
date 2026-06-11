5127ebdc5b688631a5dea53fcbbe900c2c329812
Amirali <amiralikazempour14@gmail.com@gmail.com>
2026-06-11 16:06:25 +0330

    Day2: variables, types, input, f-string + temp/age project

---
+++ temp_age.py
@@ -0,0 +1,12 @@
+print("dama ra be celcuis vared konid: ")
+dama_c = float(input())
+dama_f = dama_c*9/5+32
+print(f"dama be celcuis: {dama_c}===> dama be farenheight: {dama_f: .2f}")
+
+#=====================
+
+sale_shamsi = 1405
+print("tarikh tavalode khod ra vared konid:")
+tarikh_tavalod = int(input())
+sen = sale_shamsi-tarikh_tavalod
+print(f"senne shoma:{sen}")
---
+++ variables.py
@@ -0,0 +1,4 @@
+name = "AMIRALI"
+city = "Tehran"
+sen = 27.5
+print(f"ems: {name} , shahr: {city} sen: {sen: .2f}")
