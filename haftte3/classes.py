0a5eef9c088c482482a4ed94e74d54b4dfae2760
Amirali <amiralikazempour14@gmail.com@gmail.com>
2026-06-19 20:49:50 +0330

    Day16: OOP - state methods, __str__

--- haftte3/classes.py
+++ haftte3/classes.py
@@ -1,18 +1,19 @@
 from decimal import Decimal
-class kala:
-    def __init__(self, nam_azmayeshgah, weight, ayar):
-        self.nam_azmayeshgah = nam_azmayeshgah
-        self.weight = weight
-        self.ayar = ayar
-    def gheimat(self,gheimat_geram):
-        return self.weight * self.ayar / 750 * gheimat_geram.quantize(Decimal("0.01"))
-kala1 = kala("gil azar", Decimal("10.5"), 750)
-kala2 = kala("gillan_rey", Decimal("15.65"), 970)
-print(kala1.nam_azmayeshgah)
-print(kala1.weight)
-print(kala1.ayar)
-print(kala1.gheimat(Decimal("10000")))
-print(kala2.nam_azmayeshgah)
-print(kala2.weight)
-print(kala2.ayar)
-print(kala2.gheimat(Decimal("10000")))
+class moshtari:
+    def __init__(self, name, mande):
+        self.name = name
+        self.mande = mande
+    def kharid(self, mablagh):
+        self.mande -= mablagh
+        print(f"mablagh kharid shode={mablagh}")
+        print(f"mande nahae={self.mande}")
+    def pardakht(self, mablagh):
+        self.mande += mablagh
+        print(f"mablagh pardakht shode={mablagh}")
+        print(f"mande nahae={self.mande}")
+    def __str__(self):
+        return f"name: {self.name}, mande: {self.mande}"
+m = moshtari("amirali", Decimal("100000"))
+m.kharid(Decimal("10000"))
+m.pardakht(Decimal("100000"))
+print(m)
